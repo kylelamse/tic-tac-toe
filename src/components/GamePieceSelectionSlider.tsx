166bb3c42f2efc8a20a82fe72825958002aea012
Kyle Lamse <kylelamse29@gmail.com>
2022-02-15 16:53:49 -0600

    Allow selecting different game pieces

--- src/components/GamePieceSelectionSlider.tsx
+++ src/components/GamePieceSelectionSlider.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import GamePiece from "./GamePiece";
 
 import "./GamePieceSelectionSlider.scss";
 
+type players = "X" | "O";
+
 type Props = {};
 const GamePieceSelectionSlider = (props: Props) => {
+    const [selected, setSelected] = useState<players>("O");
+
+    const selectX = () => setSelected("X");
+    const selectO = () => setSelected("O");
+
     return (
         <div className="game_piece_selection_slider_container">
-            <GamePiece type="X" size="Medium" />
-            <GamePiece type="O" size="Medium" />
+            <div
+                onClick={selectX}
+                className={`game_piece_selection_slider_piece_container ${
+                    selected === "X" && "game_piece_selection_slider_selected"
+                }`}
+            >
+                <GamePiece type="X" size="Medium" />
+            </div>
+            <div
+                onClick={selectO}
+                className={`game_piece_selection_slider_piece_container ${
+                    selected === "O" && "game_piece_selection_slider_selected"
+                }`}
+            >
+                <GamePiece type="O" size="Medium" />
+            </div>
         </div>
     );
 };
