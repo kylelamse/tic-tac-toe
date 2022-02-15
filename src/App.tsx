2fe2fd5a25d939a5fad9e0383963389103315a4d
Kyle Lamse <kylelamse29@gmail.com>
2022-02-15 07:36:02 -0600

    Install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.scss";
 
 function App() {
-  return (
-    <main>
-      <section className="main-section">
-        <h1>Hello world!</h1>
-      </section>
-    </main>
-  );
+    return (
+        <main>
+            <section className="main-section">
+                <h1>Hello world!</h1>
+            </section>
+        </main>
+    );
 }
 
 export default App;
