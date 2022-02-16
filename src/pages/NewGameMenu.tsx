8757c3a97231ecd23da14419f8519b85ecedd1a6
Kyle Lamse <kylelamse29@gmail.com>
2022-02-15 21:16:29 -0600

    Complete layout for new game menu

--- src/pages/NewGameMenu.tsx
+++ src/pages/NewGameMenu.tsx
@@ -2,15 +2,23 @@ import Logo from "components/Logo";
 import GamePieceSelection from "components/GamePieceSelection";
 import Button from "components/Button";
 
+import "./NewGameMenu.scss";
+
 type Props = {};
 
 const NewGameMenu = (props: Props) => {
     return (
         <>
-            <Logo />
-            <GamePieceSelection />
-            <Button variant="primary">New Game (VS CPU)</Button>
-            <Button variant="secondary">New Game (VS Player)</Button>
+            <div className="new_game_menu_logo">
+                <Logo />
+            </div>
+            <div className="new_game_menu_logo_game_piece_selection">
+                <GamePieceSelection />
+            </div>
+            <div className="new_game_menu_logo_button_container">
+                <Button variant="primary">New Game (VS CPU)</Button>
+                <Button variant="secondary">New Game (VS Player)</Button>
+            </div>
         </>
     );
 };
