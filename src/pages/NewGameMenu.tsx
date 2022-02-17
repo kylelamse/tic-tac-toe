83c201eb3b7f4303b1c362eaa2781b27e4a62976
Kyle Lamse <kylelamse29@gmail.com>
2022-02-17 17:09:21 -0600

    Remove unnecessary divs and rename class

--- src/pages/NewGameMenu.tsx
+++ src/pages/NewGameMenu.tsx
@@ -9,13 +9,9 @@ type Props = {};
 const NewGameMenu = (props: Props) => {
     return (
         <div className="new_game_menu_container">
-            <div className="new_game_menu_logo">
-                <Logo />
-            </div>
-            <div className="new_game_menu_logo_game_piece_selection">
-                <GamePieceSelection />
-            </div>
-            <div className="new_game_menu_logo_button_container">
+            <Logo />
+            <GamePieceSelection />
+            <div className="new_game_menu_button_container">
                 <Button variant="primary">New Game (VS CPU)</Button>
                 <Button variant="secondary">New Game (VS Player)</Button>
             </div>
