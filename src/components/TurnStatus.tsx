cc642cf6a7fc5f341dff61cde175294e049de8d0
Kyle Lamse <kylelamse29@gmail.com>
2022-02-21 17:55:03 -0600

    Remove TurnStatus sass

--- src/components/TurnStatus.tsx
+++ src/components/TurnStatus.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import styled from "@emotion/styled";
 import GamePiece from "./GamePiece";
-import "./TurnStatus.scss";
 import Typograpy from "./Typography";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 0.5em;
+    background-color: ${(props) => props.theme.colors.semiDarkNavy};
+    border-radius: 0.3125em;
+    padding: 0.625em 1em 0.75em 1em;
+    box-shadow: inset 0 -0.25em 0 ${(props) => props.theme.colors.semiDarkNavyDropShadow};
+`;
 
 type players = "X" | "O";
 type Props = {
@@ -10,10 +19,10 @@ type Props = {
 
 const TurnStatus = ({ currentPlayer }: Props) => {
     return (
-        <div className="turn_status">
+        <Container>
             <GamePiece type={currentPlayer} size="Small" color="silver" />
             <Typograpy variant="body">Turn</Typograpy>
-        </div>
+        </Container>
     );
 };
 
