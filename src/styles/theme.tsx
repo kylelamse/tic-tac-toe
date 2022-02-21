ef732923d2f5679fd1897731ab88ea3ec1555b74
Kyle Lamse <kylelamse29@gmail.com>
2022-02-20 22:41:59 -0600

    Add theme

---
+++ src/styles/theme.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+const theme = {
+    colors: {
+        lightBlue: "hsl(178, 60%, 48%)",
+        lightBlueHover: "hsl(178, 75%, 65%)",
+        lightBlueDropShadow: "hsl(178, 78%, 31%)",
+        lightYellow: "hsl(39, 88%, 58%)",
+        lightYellowHover: "hsl(39, 100%, 69%)",
+        lightYellowDropShadow: "hsl(39, 83%, 44%)",
+        darkNavy: "hsl(202, 32%, 15%)",
+        semiDarkNavy: "hsl(199, 35%, 19%)",
+        semiDarkNavyDropShadow: "hsl(201, 45%, 11%)",
+        silver: "hsl(198, 23%, 72%)",
+        silverHover: "hsl(197, 33%, 89%)",
+        silverDropShadow: "hsl(199, 17%, 51%)",
+    },
+};
+
+export default theme;
