bdfc55603b8d973efc36c7b33cbde0b051630922
Kyle Lamse <kylelamse29@gmail.com>
2022-02-21 17:51:34 -0600

    Remove RestartButton sass

--- src/components/RestartButton.tsx
+++ src/components/RestartButton.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import "./RestartButton.scss";
+import styled from "@emotion/styled";
 import RestartIcon from "./RestartIcon";
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 2.5em;
+    max-height: 2.5em;
+    background-color: colors.$silver;
+    border-radius: 0.25em;
+    box-shadow: inset 0 -0.25em 0 #6b8997;
+    padding: 1em;
+`;
 
 type Props = {};
 
 const RestartButton = (props: Props) => {
     return (
-        <div className="restart_button_container">
+        <Container>
             <RestartIcon />
-        </div>
+        </Container>
     );
 };
 
