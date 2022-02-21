1952c5f56437ba0e85d19fe523a4b1ae570c081c
Kyle Lamse <kylelamse29@gmail.com>
2022-02-21 17:50:03 -0600

    Remove logo sass

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,21 @@
-import "./Logo.scss";
+import styled from "@emotion/styled";
 import GamePiece from "components/GamePiece";
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+`;
 
 type Props = {};
 
 const Logo = (props: Props) => {
     return (
-        <div className="logo_container">
+        <Container>
             <GamePiece type="X" size="Medium" />
             <GamePiece type="O" size="Medium" />
-        </div>
+        </Container>
     );
 };
 
