5acac24968e0b86b1b87084d6a13c1d9864723c3
Kyle Lamse <kylelamse29@gmail.com>
2022-02-23 20:59:54 -0600

    Add breakpoint hook

---
+++ src/hooks/useBreakpoint.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+type Breakpoint = {
+    min: string;
+    max: string;
+};
+
+export default function useBreakpoint({ min = "", max = "" }: Breakpoint) {
+    const [match, setMatch] = useState(false);
+
+    useEffect(() => {
+        let query = "";
+        if (min && max) {
+            query = `(max-width: ${max}) and (min-width: ${min})`;
+        } else if (min) {
+            query = `(min-width: ${min})`;
+        } else if (max) {
+            query = `(max-width: ${max})`;
+        }
+
+        if (query) {
+            const mediaQueryList = window.matchMedia(query);
+
+            mediaQueryList.addEventListener("change", (event) => {
+                setMatch(event.matches);
+            });
+        }
+    });
+
+    return match;
+}
