6ade10e6d4dd35780ad8e167c69e184b8e6f83c4
Kyle Lamse <kylelamse29@gmail.com>
2022-02-28 12:44:33 -0600

    Add game piece selection hover states

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -7,6 +7,7 @@ const theme = {
         lightYellowHover: "hsl(39, 100%, 69%)",
         lightYellowDropShadow: "hsl(39, 83%, 44%)",
         darkNavy: "hsl(202, 32%, 15%)",
+        darkNavyHover: "rgba(168, 191, 201, 0.05)",
         semiDarkNavy: "hsl(199, 35%, 19%)",
         semiDarkNavyDropShadow: "hsl(201, 45%, 11%)",
         silver: "hsl(198, 23%, 72%)",
