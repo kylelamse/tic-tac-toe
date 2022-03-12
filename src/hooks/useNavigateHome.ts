c1b4a50b3a4ea623f7fd6d090c8e330b61fd7160
Kyle Lamse <kylelamse29@gmail.com>
2022-03-11 21:46:30 -0600

    Reset player one on quit

--- src/hooks/useNavigateHome.ts
+++ src/hooks/useNavigateHome.ts
@@ -1,16 +1,19 @@
 import { useCallback } from "react";
 import { useResetRecoilState } from "recoil";
 import page from "state/atoms/page";
+import playerOneState from "state/atoms/playerOneState";
 import useResetGame from "./useResetGame";
 
 const useNavigateHome = () => {
     const resetGame = useResetGame();
     const resetPage = useResetRecoilState(page);
+    const resetPlayerOne = useResetRecoilState(playerOneState);
 
     const navigateHome = useCallback(() => {
         resetGame();
         resetPage();
-    }, [resetGame, resetPage]);
+        resetPlayerOne();
+    }, [resetGame, resetPage, resetPlayerOne]);
     return navigateHome;
 };
 
