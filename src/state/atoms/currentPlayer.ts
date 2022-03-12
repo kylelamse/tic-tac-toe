11af80f1fb6baceff97a6031bca07c9ede5fa5e8
Kyle Lamse <kylelamse29@gmail.com>
2022-03-11 18:47:32 -0600

    Allow placing game pieces and switching turns

--- src/state/atoms/currentPlayer.ts
+++ src/state/atoms/currentPlayer.ts
@@ -3,7 +3,7 @@ import { atom } from "recoil";
 type players = "O" | "X";
 const currentPlayer = atom<players>({
     key: "currentPlayer",
-    default: "O",
+    default: "X",
 });
 
 export default currentPlayer;
--- src/state/atoms/gamePiecePlacement.ts
+++ src/state/atoms/gamePiecePlacement.ts
@@ -4,9 +4,9 @@ import { atom } from "recoil";
 const gamePiecePlacement = atom<board>({
     key: "gamePiecePlacement",
     default: [
-        ["", "X", "O"],
-        ["X", "O", "X"],
-        ["", "O", "X"],
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
     ],
 });
 
